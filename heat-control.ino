749da0a0d520fcb7d173bae329001845bbd40ea0
faolanbig <faolan.big@web.de>
2026-02-03 08:35:29 +0100

    update

--- heat-control.ino
+++ heat-control.ino
@@ -1,9 +1,20 @@
+#define TEMP_SENSOR_PIN A0
+
 void setup()
 {
-	
+    Serial.begin(9600);
+    pinMode(TEMP_SENSOR_PIN, INPUT);
 }
 
 void loop()
 {
-	
+	int tempSensorValue = analogRead(TEMP_SENSOR_PIN);
+    float voltage = tempSensorValue * (5.0 / 1023.0);
+    float temperatureC = (voltage - 0.5) * 100.0;
+
+    Serial.print("Temperature: ");
+    Serial.print(temperatureC);
+    Serial.println(" °C");
+
+    delay(1000);
 }
